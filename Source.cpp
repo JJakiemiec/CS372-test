723d3c000886cb2b64c61be8e3689732f05bee80
Jacob Jakiemiec <jjakiemiec@gmail.com>
2021-01-20 13:27:41 -0900

    add more random commands

--- Source.cpp
+++ Source.cpp
@@ -7,10 +7,14 @@
 int main() {
 
 	std::cout << "It do" << std::endl;
+	std::cout << "why are you like this" << std::endl;
 	int num = 78;
 	num++;
 	num++;
 	if (num > 1)
 		std::cout << "wow" << std::endl;
 	cout << "this is in main, and was a feature" << std::endl;
+	char letter = 'c';
+	letter++;
+	std::cout << letter << std::endl;
 }
